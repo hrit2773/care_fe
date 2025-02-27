a8d1d7f90cebefc0793ad27573d13e6aa7e4659c
Hritesh Shanty <shanty.hritesh@gmail.com>
2025-02-27 16:24:37 +0530

    valueset options fetching

--- src/components/Questionnaire/QuestionTypes/ChoiceQuestion.tsx
+++ src/components/Questionnaire/QuestionTypes/ChoiceQuestion.tsx
@@ -1,5 +1,7 @@
-import { memo } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { memo, useState } from "react";
 
+import Autocomplete from "@/components/ui/autocomplete";
 import {
   Select,
   SelectContent,
@@ -8,6 +10,8 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
+import routes from "@/Utils/request/api";
+import query from "@/Utils/request/query";
 import { properCase } from "@/Utils/utils";
 import type {
   QuestionnaireResponse,
@@ -29,6 +33,32 @@ interface ChoiceQuestionProps {
   index?: number;
 }
 
+function ValuesetAutoComplete(props: { valueset: string }) {
+  const [search, setSearch] = useState("");
+
+  const fetchValuesetOptions = useQuery({
+    queryKey: ["valueset", props.valueset, "expand", search],
+    queryFn: query.debounced(routes.valueset.expand, {
+      pathParams: { system: props.valueset },
+      body: { search: "", count: 10 },
+    }),
+  });
+  return (
+    <Autocomplete
+      options={
+        fetchValuesetOptions.data?.results.map((option) => {
+          return {
+            label: option.display!,
+            value: option.code,
+          };
+        }) || []
+      }
+      value={search}
+      onChange={(value: string) => setSearch(value)}
+    />
+  );
+}
+
 export const ChoiceQuestion = memo(function ChoiceQuestion({
   question,
   questionnaireResponse,
@@ -37,9 +67,11 @@ export const ChoiceQuestion = memo(function ChoiceQuestion({
   clearError,
   index = 0,
 }: ChoiceQuestionProps) {
+  if (question.answer_value_set) {
+    return <ValuesetAutoComplete valueset={question.answer_value_set} />;
+  }
   const options = question.answer_option || [];
   const currentValue = questionnaireResponse.values[index]?.value?.toString();
-
   const handleValueChange = (newValue: string) => {
     clearError();
     const newValues = [...questionnaireResponse.values];
